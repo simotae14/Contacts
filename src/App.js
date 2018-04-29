4ba7e2d821f68e8c11a593507354aecf33542d3a
simotae14 <startae14@gmail.com>
2018-04-29 21:11:35 +0200

    State Management Introduction

--- src/App.js
+++ src/App.js
@@ -1,34 +1,10 @@
 import React, { Component } from 'react';
 
-// creo ContactList
-class ContactList extends Component {
-  render() {
-    const people = this.props.contacts;
-
-    return <ol>
-      { people.map((person) => (
-        <li key={person.name}>{person.name}</li>
-      ))}
-    </ol>;
-  }
-}
-
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <ContactList contacts={[
-          { name: 'Tyler' },
-          { name: 'Karen' },
-          { name: 'Richard' }
-        ]} />
-        <ContactList contacts={[
-          { name: 'Amanda' },
-          { name: 'Mikenzi' },
-          { name: 'Ryan' }
-        ]} />
-      </div>
-    );
+      return (
+        <div>Hello World</div>
+      )
   }
 }
 
