01786594680144a8305a11fd31192c1c91afacee
Simone Taeggi <simone.taeggi@leovegas.com>
2018-05-07 15:14:41 +0200

    Dinamically Render Pages part1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts';
 import * as ContactsAPI from './utils/ContactsAPI';
+import CreateContact from './CreateContact';
 
 class App extends Component {
   state = {
-    contacts:  []
+    contacts:  [],
+    screen: 'create'
   }
   // recupero i dati
   componentDidMount() {
@@ -28,10 +30,15 @@ class App extends Component {
   render() {
       return (
         <div>
-          <ListContacts
-            contacts={this.state.contacts}
-            onDeleteContact={this.removeContact}
-          />
+          {this.state.screen === 'list' &&
+            <ListContacts
+              contacts={this.state.contacts}
+              onDeleteContact={this.removeContact}
+              />
+          }
+          {this.state.screen === 'create' &&
+            <CreateContact />
+          }
         </div>
       )
   }
